0f4de11580c38535ecc12c89c4ec04c978ba4c24
IsraelBain <144736476+IsraelBain@users.noreply.github.com>
2025-03-06 13:30:47 -0400

    Testing123(Navbar)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import logo from "../assets/logo.jpg"; // Ensure the path is correct
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 flex justify-between items-center px-10 py-4 font-sans">
+    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 flex justify-between items-center px-4 md:px-10 py-4 font-sans">
       {/* Clickable Logo */}
       <Link to="/" className="flex items-center">
         <img src={logo} alt="Two Sailors Bartending Logo" className="h-24 w-36 rounded-lg cursor-pointer mr-4 md:mr-0" />
       </Link>
 
       {/* Navigation Links */}
-      <ul className="nav-links flex gap-6 text-lg font-semibold tracking-wide font-sans">
+      <ul className="nav-links flex gap-4 md:gap-6 text-lg font-semibold tracking-wide font-sans">
         <li>
           <Link to="/" className="hover:text-orange-500 text-lg font-sans">
             Home
