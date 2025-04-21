f4d73b893ae43129384143f57dd63f71e00e43c0
IsraelBain <144736476+IsraelBain@users.noreply.github.com>
2025-04-21 18:30:06 -0300

    changed nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.png";
 import facebookIcon from "../assets/facebook.png";
 import instagramIcon from "../assets/instagram.png";
 import tiktokIcon from "../assets/tiktok.png";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 flex justify-between items-center px-4 md:px-10 py-4 font-sans">
-      {/* Left: Social Icons */}
-      <div className="flex items-center gap-4">
-        <a
-          href="https://www.tiktok.com/@twosailorsbartending"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={tiktokIcon} alt="TikTok" className="h-6 w-6" />
-        </a>
-        <a
-          href="https://www.instagram.com/twosailorsbartending/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={instagramIcon} alt="Instagram" className="h-6 w-6" />
-        </a>
-        <a
-          href="https://www.facebook.com/profile.php?id=61554763772561"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={facebookIcon} alt="Facebook" className="h-6 w-6" />
-        </a>
+    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 py-4 font-sans">
+      <div className="flex items-center justify-between px-4 md:px-10 relative">
+        {/* Left: Social Icons */}
+        <div className="flex items-center gap-4">
+          <a href="https://www.tiktok.com/@twosailorsbartending" target="_blank" rel="noopener noreferrer">
+            <img src={tiktokIcon} alt="TikTok" className="h-8 w-8" />
+          </a>
+          <a href="https://www.instagram.com/twosailorsbartending/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramIcon} alt="Instagram" className="h-8 w-8" />
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61554763772561" target="_blank" rel="noopener noreferrer">
+            <img src={facebookIcon} alt="Facebook" className="h-8 w-8" />
+          </a>
+        </div>
+
+        {/* Center: Logo */}
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Two Sailors Bartending Logo"
+              className="h-20 rounded-lg cursor-pointer"
+            />
+          </Link>
+        </div>
+
+        {/* Right: Nav Links */}
+        <ul className="flex gap-4 md:gap-6 text-lg font-semibold tracking-wide font-sans">
+          <li>
+            <Link to="/" className="hover:text-orange-500">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-orange-500">About Us</Link>
+          </li>
+          <li>
+            <Link to="/blog" className="hover:text-orange-500">Blog</Link>
+          </li>
+        </ul>
       </div>
-
-      {/* Center: Logo */}
-      <Link to="/" className="absolute left-1/2 transform -translate-x-1/2">
-        <img
-          src={logo}
-          alt="Two Sailors Bartending Logo"
-          className="h-16 rounded-lg cursor-pointer"
-        />
-      </Link>
-
-      {/* Right: Navigation Links */}
-      <ul className="flex gap-4 md:gap-6 text-lg font-semibold tracking-wide font-sans">
-        <li>
-          <Link to="/" className="hover:text-orange-500 text-lg font-sans">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="hover:text-orange-500 text-lg font-sans">
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link to="/blog" className="hover:text-orange-500 text-lg font-sans">
-            Blog
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
