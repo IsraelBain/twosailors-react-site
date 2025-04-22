cf73f93bc87c0530683f5a002b83dcafeab75a63
IsraelBain <144736476+IsraelBain@users.noreply.github.com>
2025-04-21 21:53:56 -0300

    idk navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,25 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 py-6 md:py-8 font-sans">
+    <nav className="bg-blue-900 text-white fixed top-0 w-full z-50 py-5 md:py-8 font-sans">
       <div className="flex items-center justify-between px-4 md:px-10 relative">
         {/* Left: Social Icons */}
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-2 md:gap-3">
           <a href="https://www.tiktok.com/@twosailorsbartending" target="_blank" rel="noopener noreferrer">
-            <img src={tiktokIcon} alt="TikTok" className="h-8 w-8 md:h-12 md:w-12" />
+            <img src={tiktokIcon} alt="TikTok" className="h-6 w-6 md:h-10 md:w-10" />
           </a>
           <a href="https://www.instagram.com/twosailorsbartending/" target="_blank" rel="noopener noreferrer">
-            <img src={instagramIcon} alt="Instagram" className="h-8 w-8 md:h-12 md:w-12" />
+            <img src={instagramIcon} alt="Instagram" className="h-6 w-6 md:h-10 md:w-10" />
           </a>
           <a href="https://www.facebook.com/profile.php?id=61554763772561" target="_blank" rel="noopener noreferrer">
-            <img src={facebookIcon} alt="Facebook" className="h-8 w-8 md:h-12 md:w-12" />
+            <img src={facebookIcon} alt="Facebook" className="h-6 w-6 md:h-10 md:w-10" />
           </a>
         </div>
 
         {/* Center: Logo */}
         <div className="absolute left-1/2 transform -translate-x-1/2">
           <Link to="/">
-            <img src={logo} alt="Two Sailors Bartending Logo" className="h-16 md:h-20 rounded-lg cursor-pointer" />
+            <img src={logo} alt="Two Sailors Bartending Logo" className="h-12 md:h-20 rounded-lg cursor-pointer" />
           </Link>
         </div>
 
@@ -55,7 +55,7 @@ const Navbar = () => {
 
       {/* Mobile Dropdown */}
       {isMobileMenuOpen && (
-        <div className="md:hidden bg-blue-800 px-6 pb-4">
+        <div className="md:hidden bg-blue-800 px-6 pt-4 pb-4">
           <ul className="flex flex-col space-y-3 text-lg font-semibold">
             <li><Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
             <li><Link to="/about" onClick={() => setIsMobileMenuOpen(false)}>About Us</Link></li>
